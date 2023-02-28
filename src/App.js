e5621d9775c507a64a7b524648b577332ba97fc1
Mina Sameh <mina.sameh.lameh@gmail.com>
2023-02-28 16:16:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {CKEditor} from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {useCallback, useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState('')
+    const [currentEditor, setCurrentEditor] = useState(null)
+
+    const removeLines = useCallback(() => {
+        if (!currentEditor) return
+        currentEditor.setData(data.replaceAll('<p>&nbsp;</p>', ''))
+    }, [currentEditor, data])
+
+    const keydownHandler = useCallback(function (e) {
+        if (e.keyCode === 13 && e.ctrlKey) removeLines()
+    }, [removeLines])
+
+    useEffect(() => {
+        document.addEventListener('keydown', keydownHandler);
+        return () => {
+            document.removeEventListener('keydown', keydownHandler);
+        }
+    }, [keydownHandler])
+
+    return (
+        <div style={{width: "100%", display: "flex"}}>
+            <CKEditor
+                editor={ClassicEditor}
+                data={data}
+                onReady={(editor) => {
+                    setCurrentEditor(editor)
+                }}
+                onChange={(event, editor) => {
+                    setData(editor.getData())
+                }}
+            />
+            <button onClick={() => {
+                removeLines()
+            }} style={{marginLeft: "50px", padding: "10px 20px"}}>
+                Remove break lines
+            </button>
+        </div>
+    );
 }
 
 export default App;
